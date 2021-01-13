59480fea3ead9730769cd551d576ed1c395df2ea
Andrii <bezkrovnyyav@gmail.com>
2021-01-13 19:26:46 +0200

    Add content

---
+++ assets/js/main.js
@@ -0,0 +1,27 @@
+let clickCount = 0;
+
+window.onload = function () {
+    enableGameTetris();
+    enableVideoEasterEgg();
+};
+
+
+function enableVideoEasterEgg() {
+    var easterEgg = document.getElementsByClassName('avatar-wrapper')[0];
+    easterEgg.onclick = function () {
+        setTimeout(function () {
+            clickCount++;
+            document.getElementsByTagName('audio')[0].play();
+        }, 500);
+        if (clickCount === 2) {
+            window.open('https://www.youtube.com/watch?v=0i1RcEHk0f4', '_blank');
+        } 
+    };
+}
+
+function enableGameTetris() {
+    let easterEgg = document.getElementById('Tetris');
+    easterEgg.onclick = function () {
+        window.open('https://www.min2win.ru/gm.php?id=3336', '_blank');
+    };
+}
